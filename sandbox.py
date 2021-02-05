ace07e5c45cac70ab0c57af523bafbc3e249beca
Nikos Kampouridis <nkampouridis9@gmail.com>
2021-02-05 23:05:21 +0200

    Removed some code

--- sandbox.py
+++ sandbox.py
@@ -10,9 +10,5 @@ employee_1.last = "Schafer"
 employee_1.email = "CoreyMSchafer@gmail.com"
 employee_1.pay = 50000
 
-# employee_2.first = 'Test'
-# employee_2.last = 'Employee'
-# employee_2.email = 'Test.Employee@demo.com'
-# employee_2.pay = 70000
+print(employee_1.first)
 
-print(employee_1.email)
