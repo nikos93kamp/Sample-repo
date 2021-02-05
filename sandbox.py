dee2a1e4d7aec17f16146a70912d34315a45cf56
Nikos Kampouridis <nkampouridis9@gmail.com>
2021-02-05 23:00:29 +0200

    Initial Commit

---
+++ sandbox.py
@@ -0,0 +1,18 @@
+class Employee:
+    pass
+
+
+employee_1 = Employee()
+employee_2 = Employee()
+
+employee_1.first = "Corey"
+employee_1.last = "Schafer"
+employee_1.email = "CoreyMSchafer@gmail.com"
+employee_1.pay = 50000
+
+# employee_2.first = 'Test'
+# employee_2.last = 'Employee'
+# employee_2.email = 'Test.Employee@demo.com'
+# employee_2.pay = 70000
+
+print(employee_1.email)
